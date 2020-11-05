82766d5291a23b2bdc15be2e36200d5d56ba730c
Carrie <kaka784804@gmail.com>
2020-11-06 00:19:56 +0800

    tttttt

--- vue.config.js
+++ vue.config.js
@@ -11,7 +11,7 @@ module.exports = {
             '/api': {
                 target: 'http://18.190.3.152/',
                 ws: true,
-                changeOrigin: true,
+                // changeOrigin: true,
                 pathRewrite: {
                     '^/api': '', //思路是如果是开发环境，就给所有要代理的接口统一加上前缀，然后代理请求时再统一通过rewrite去掉
                 }
