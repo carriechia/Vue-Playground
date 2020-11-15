37cff124757344f5ee82ad2110c35b5454ef9892
Carrie <kaka784804@gmail.com>
2020-11-15 23:18:36 +0800

    add check form

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -22,6 +22,7 @@
 
 <script>
 import {login} from '@/api/member'
+import {checkForm} from '@/api/member'
 
   export default {
     data() {
@@ -34,6 +35,19 @@ import {login} from '@/api/member'
     },
     methods: {
       Singin() {
+      Singin: function(e) {
+        e.preventDefault();
+        var self = this;
+        const member = {
+            email: self.form.email,
+            password: self.form.password
+        }
+        var messageArray = checkForm(member);
+        if (messageArray.length > 0) {
+            this.notifyVue(messageArray.join("<br/>"), 'danger');
+            return false;
+        }
+
         var self = this;
         const member = {
             email: self.form.email,
